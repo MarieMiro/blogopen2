a530288fe222e2582c5fcfe117f064213ad0b189
maria <mariayushko61@gmail.com>
2026-01-21 17:37:25 +0700

    settings database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -72,13 +72,23 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 # ===== DB =====
-DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ["DATABASE_URL"],
-        conn_max_age=600,
-        ssl_require=True,  # для render/postgres обычно нужно
-    )
-}
+DATABASE_URL = os.getenv("DATABASE_URL")  # может быть пусто локально
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=DATABASE_URL.startswith("postgres"),
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # ===== CORS/CSRF =====
 def _split_env_urls(name: str):
