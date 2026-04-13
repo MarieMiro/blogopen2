8782b493a2f8341ed0fbca788eb97a6e002ec1b8
maria <mariayushko61@gmail.com>
2026-04-13 13:45:35 +0700

    superuser

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-import os
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -7,27 +6,3 @@ class AccountsConfig(AppConfig):
 
     def ready(self):
         import accounts.signals
-
-        # чтобы выполнялось только на Render
-        if os.environ.get("RENDER") != "true":
-            return
-
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-        if not username or not email or not password:
-            return
-
-        try:
-            from django.contrib.auth.models import User
-
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(
-                    username=username,
-                    email=email,
-                    password=password,
-                )
-                print("✅ Superuser created")
-        except Exception as e:
-            print("❌ Error creating superuser:", e)
